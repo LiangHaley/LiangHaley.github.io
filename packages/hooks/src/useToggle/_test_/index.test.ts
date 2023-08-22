c1190cb885b3e1927230707761f727b7b0adc68c
LiangHaley <133198031+LiangHaley@users.noreply.github.com>
2023-08-22 14:37:18 +0800

    Update index.test.ts
    删除jest 部分报错

--- packages/hooks/src/useToggle/_test_/index.test.ts
+++ packages/hooks/src/useToggle/_test_/index.test.ts
@@ -16,12 +16,4 @@ describe('useToggle',()=>{
         const hooks = renderHook(()=>useToggle());
         expect(hooks.result.current[0]).toBe(false);
     })
-    it('针对手动切换toggle模拟',()=>{
-        const hooks = renderHook(()=> useToggle("haleyliang","lianghaley"))
-        expect(hooks.result.current[0]).toBe("haleyliang");
-        callToggle(hooks)
-        expect(hooks.result.current[0]).toBe("lianghaley");
-        callToggleLeft(hooks)
-        expect(hooks.result.current[0]).toBe("lianghaley");
-    })
 })
