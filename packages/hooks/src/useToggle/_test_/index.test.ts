b7694a4347cb2da402b6af13b4c38248712292a4
LiangHaley <133198031+LiangHaley@users.noreply.github.com>
2023-08-22 14:46:13 +0800

    Update index.test.ts
    2222

--- packages/hooks/src/useToggle/_test_/index.test.ts
+++ packages/hooks/src/useToggle/_test_/index.test.ts
@@ -1,10 +1,5 @@
 import {renderHook} from '@testing-library/react'
 import useToggle from "../index";
-const callToggle = (hook)=>{
-    act(()=>{
-        hook.result.current[1].toggle()
-    })
-}
 describe('useToggle',()=>{
     //jest
     it('针对基础功能使用测试',()=>{
